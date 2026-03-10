3baab0143e01c1bed95a9c9e56e59d7512668154
Abishiyam Baskaran <121865996+Abishiyambas@users.noreply.github.com>
2026-03-10 17:24:21 +0100

    initial commit

--- Core/src/main/java/dk/sdu/cbse/App.java
+++
@@ -1,8 +0,0 @@
-package dk.sdu.cbse;
-
-public class App {
-    public static void main(String[] args) {
-        // Module entry point; add your app startup here.
-        System.out.println("Hello, Core!");
-    }
-}
